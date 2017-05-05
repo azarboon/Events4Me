ba2b72d34742ae74fe9aed362d10d1cafda08039
fourtti <fourtti@gmail.com>
2017-05-05 15:20:50 +0300

    modified myprofile and script

--- src/main/resources/static/js/scriptprofile.js
+++ src/main/resources/static/js/scriptprofile.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
     if ($("#attending").children().length === 0) {
         $("#attending").hide();
+
+        var p = document.createElement('p');
+        p.setAttribute("id", "notattending");
+        p.style = "text-align: center";
+        p.innerHTML = "You are not attending any events currently";
+        document.getElementById('notattending').appendChild(p);
+
+
     }
 
 });
