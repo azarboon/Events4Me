f95d9cf76c6b7b3360a759cbed7fd04d507ad4db
Dmitry Khramov <dmitrii.hramov@mail.ru>
2017-05-05 12:30:35 +0300

    tests fixed

--- src/test/java/com/metropolia/events4me/Service/EventUserServiceImplTest.java
+++ src/test/java/com/metropolia/events4me/Service/EventUserServiceImplTest.java
@@ -1,52 +1,54 @@
-//package com.metropolia.events4me.Service;
-//
-//import com.metropolia.events4me.Model.Event;
-//import com.metropolia.events4me.Model.User;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//
-///**
-// * Created by Dmitry on 18.04.2017.
-// */
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
-////@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
-//public class EventUserServiceImplTest {
-//
-//    private EventUserService eventUserService;
-//    private UserService userService;
-//    private EventService eventService;
-//
-//    @Autowired
-//    public void setEventUserService(EventUserService eventUserService) {
-//        this.eventUserService = eventUserService;
-//    }
-//
-//    @Autowired
-//    public void setUserService(UserService userService) {
-//        this.userService = userService;
-//    }
-//
-//    @Autowired
-//    public void setEventService(EventService eventService) {
-//        this.eventService = eventService;
-//    }
-//
-//    @Test
-//    @Transactional
-//    public void matchEventsForUserTest() throws Exception {
-//        User user = userService.findByUsername("dima");
-//        List<Event> list = eventUserService.matchEventsForUser(user);
-//        System.out.println(list.size());
-//        assert list.size() == 2;
-//    }
-//
+package com.metropolia.events4me.Service;
+
+import com.metropolia.events4me.Model.Event;
+import com.metropolia.events4me.Model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Created by Dmitry on 18.04.2017.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class EventUserServiceImplTest {
+
+    private EventUserService eventUserService;
+    private UserService userService;
+    private EventService eventService;
+
+    @Autowired
+    public void setEventUserService(EventUserService eventUserService) {
+        this.eventUserService = eventUserService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void setEventService(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @Test
+    @Transactional
+    public void matchEventsForUserTest() throws Exception {
+        User user = userService.findByUsername("dima");
+        List<Event> list = eventUserService.matchEventsForUser(user);
+        System.out.println(list.size());
+        assert list.size() == 3;
+    }
+
 //    @Test
 //    @Transactional
 //    public void joinEventTest() throws Exception {
@@ -60,4 +62,4 @@
 //        }
 //        assert counter == 3;
 //    }
-//}
+}
