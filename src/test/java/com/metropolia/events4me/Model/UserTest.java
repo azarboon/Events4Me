b1b4e5947b9a09fdf7f5dc6aa37f70fe759b0687
Dmitry Khramov <dmitrii.hramov@mail.ru>
2017-05-05 16:25:06 +0300

    Test cloud

--- src/test/java/com/metropolia/events4me/Model/UserTest.java
+++ src/test/java/com/metropolia/events4me/Model/UserTest.java
@@ -11,11 +11,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class UserTest {
 
     @Mock
