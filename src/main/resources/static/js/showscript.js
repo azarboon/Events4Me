5cbec837f230bf16fb72ee29c96eda99716c5406
theonelegion <niklas.kuusisto95@gmail.com>
2017-05-10 16:09:57 +0300

    fixed bug related to joining events

--- src/main/resources/static/js/showscript.js
+++ src/main/resources/static/js/showscript.js
@@ -8,7 +8,6 @@ $(document).ready(function () {
             type: "POST",
             url: "/join/event/" + urlArray,
             success: function(data) {
-                console.log(data);
 
 
             }
